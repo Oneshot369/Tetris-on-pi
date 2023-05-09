3994cc791e0e05b0044561b0cae29f6572767317
Oneshot369 <joshcpeck4@gmail.com>
2023-05-09 10:17:29 -0700

    adding 2d array features instead of list

--- tetris.py
+++ tetris.py
@@ -2,7 +2,8 @@
 from sense_emu import SenseHat
 import time
 
-#lets Define some vars
+################################################
+    #lets Define some vars
 row = 8
 bottom_row = 56
 speed = 2
@@ -14,21 +15,68 @@ reset = (0, 0, 0)
 
 #This is our SenseHat object
 sense = SenseHat()
-#define some methods
+
+#this is our 2 d array 64 lights
+ourArr = [[0 for x in range(row)] for y in range(row)]
+
+#######################################################
+    #define some methods
+#move mothod - this does not work yet
 def move(event):
-    #move right
-    if event.direction == "right":
-        ourPixles[index] = reset
-        ourPixles[index + 1] = white
-    else:
-        ourPixles[index] = reset
-        ourPixles[index + 1] = white
+    print("move method")
+    #get our list
+    ourPixles = sense.get_pixels()
+    printList(ourPixles)
+    for x in ourPixles:
+        if x == white:
+            #move right
+            if event.direction == "right":
+                print("moving right")
+                ourPixles[x] = reset
+                ourPixles[x + 1] = white
+            else:
+                print("moving left")
+                ourPixles[x] = reset
+                ourPixles[x - 1] = white
+
+#prints out our list of pixles
+def printList(pixleList):
+    for i in pixleList:
+        print(i)
+#converts a list into a 2d array
+def convert2D(ourList):
+    #vars
+    x, y = 0, 0
+    temArr = [[0 for x in range(row)] for y in range(row)]
+    
+    #loop thru our list
+    for i in ourList:
+        #copy each index from our list into temArr
+        temArr[x][y] = i
+        y += 1
+        
+        #one we reach the end of our row, set x back to zero and increase y by 1
+        if y == row:
+            y = 0
+            x += 1
+            
+    #return our array
+    return temArr
+# gets a list from the sense hat and converts to a 2darray
+def getArr():
+    x = 1
+##############################################################
+    # the 'main'
+#Clear out the sense hat
+sense.clear()
+
+ourArr = convert2D(sense.get_pixels())
+printList(ourArr)
+
 #define what happens when the user presses left or right
 sense.stick.direction_left = move
 sense.stick.direction_right = move
 
-#Clear out the sense hat
-sense.clear()
 
 #Has tetris scroll across the screen
 sense.show_message("Tetris", 0.1 ,(255, 0, 0))
